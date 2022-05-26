41f452eec1076a86cd6e9cd6331473eabe20c134
Gabriel Wright <gabriel.wright@ryerson.ca>
2022-05-26 13:13:04 -0400

    added user selected number of samples and random generated list of lines

--- script.js
+++ script.js
@@ -4,12 +4,51 @@
 // Demonstrate the sorting happening on the screen using animations/transitions
 // Indicate to the user when the sorting starts/ends
 
-// Fill up the samples
+app = {};
 
-const sampleContainer = document.querySelector(".sample-container");
+app.clearSamples = (sampleContainer) => {
+	// loop over samples in container and remove each one
+	while (sampleContainer.firstChild) {
+		sampleContainer.removeChild(sampleContainer.firstChild);
+	}
+};
 
-const sampleLine = document.createElement("li");
+app.generateSamples = () => {
+	// Fill up the samples
+	const sampleContainer = document.querySelector(".sample-container");
+	const numSamples = document.querySelector("#num-samples");
 
-sampleLine.setAttribute("id");
+	// clear current list of samples
+	app.clearSamples(sampleContainer);
 
-console.log(sampleLine);
+	// generate random line length
+	const randomizeLength = (max, min) => {
+		return Math.floor(Math.random() * (max - min) + min);
+	};
+
+	// Generate samples and append to container
+	for (let i = 0; i <= numSamples.value; i++) {
+		// target each sample line
+		const sampleLine = document.createElement("li");
+		// give each sample a unique id
+		sampleLine.setAttribute("id", i);
+		// set each sample's lengths randomly
+		const randomLineLength = randomizeLength(10, 400);
+		sampleLine.style.width = `${randomLineLength}px`;
+		sampleContainer.append(sampleLine);
+	}
+};
+
+app.setupSampleButton = () => {
+	// target generate sample button
+	const sampleBtn = document.querySelector("#sample-generator");
+	sampleBtn.addEventListener("click", () => {
+		app.generateSamples();
+	});
+};
+
+app.init = () => {
+	app.setupSampleButton();
+};
+
+app.init();
